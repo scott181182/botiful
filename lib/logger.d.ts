456f5497845ef3a38be7e76b53f7be8a9f3599f3
Scott Fasone <scott181182@yahoo.com>
2019-09-15 13:52:12 -0400

    Updated packages and dependencies

--- lib/logger.d.ts
+++ lib/logger.d.ts
@@ -1,4 +1,4 @@
 import { Logger } from "winston";
 import { IDiscordBotConfigComplete } from "./config";
-export declare const bot_format: import("../../../../../../../Users/scottfasone/Documents/Programming/Javascript/botiful/node_modules/logform").FormatWrap;
+export declare const bot_format: import("logform").FormatWrap;
 export declare function init_logger(config: IDiscordBotConfigComplete): Logger;
